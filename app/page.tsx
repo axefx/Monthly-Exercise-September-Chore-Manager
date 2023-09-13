902319f378df4c1ba863b657f99c224c0278488e
Axel <axacorro@gmail.com>
2023-09-12 18:05:13 -0700

    Updates page.tsx with initial header using tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <h1 className="text-5xl font-bold  text-center border-black border-solid border-b">
+      Planner
+    </h1>
+  );
 }
