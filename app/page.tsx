a76701646fd371138ee4348b62d7119221660533
Axel <axacorro@gmail.com>
2023-09-20 14:41:49 -0700

    Updates page.tsx to import and render Tasks component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Tasks from "./Tasks";
 
 export default function Page() {
   return (
-    <h1 className="text-5xl font-bold  text-center border-black border-solid border-b">
-      Planner
-    </h1>
+    <>
+      <h1 className="text-5xl font-bold  text-center border-black border-solid border-b">
+        Planner
+      </h1>
+      <Tasks />
+    </>
   );
 }
